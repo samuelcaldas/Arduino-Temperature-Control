edf7e7240fe22e56694c11f627c675a9e1e829fa
samuelcaldas <samuel.caldas@gmail.com>
2024-02-15 05:17:17 -0300

    Update source to use DHT.h

--- src/Aula_Pratica_7/Aula_Pratica_7.ino
+++ src/Aula_Pratica_7/Aula_Pratica_7.ino
@@ -1,17 +1,30 @@
-// Aula Prática 7
-// - Entre -40ºC e 25ºC o LED azul deve permanecer totalmente aceso;
-// - Entre 25ºC e 60ºC o LED amarelo deve aumentar seu brilho gradualmente conforme o aumento da temperatura;
-// - Entre 60ºC e 100ºC o LED vermelho deve piscar à uma frequência de 5 Hz indicando alerta;
-// - Se ultrapassar os 100ºC todos os LED's devem acender e o sistema deverá ser desativado;
-// - O sistema só retornará quando o botão for pressionado e a temperatura estiver abaixo dos 25ºC;
-// - Utilize o canal serial para exibir a temperatura de leitura no monitor serial;
+/**
+ * @file Aula_Pratica_7.ino
+ * @brief Aula Prática 7 - Sistema de alerta de controle de temperatura
+ * @version 1.0
+ * @date 2023-06-04
+ *
+ * A. O roteiro envolve o desenvolvimento de um sistema de alerta de controle de temperatura utilizando o Arduino.
+ * B. O sistema deve conter um sensor de temperatura, um botão e três LED's (um azul, um amarelo e um vermelho).
+ * C. Em temperaturas entre -40ºC e 25ºC o LED azul deve permanecer totalmente aceso.
+ * D. Em temperaturas entre 25ºC e 60ºC o LED amarelo deve aumentar seu brilho gradualmente conforme o aumento da temperatura.
+ * E. Em temperaturas entre 60ºC e 100ºC o LED vermelho deve piscar à uma frequência de 5 Hz indicando alerta.
+ * F. Se a temperatura ultrapassar os 100ºC todos os LED's devem acender e o sistema deverá ser desativado.
+ * G. O sistema só retornará quando o botão for pressionado e a temperatura estiver abaixo dos 25ºC.
+ * H. Utilize o canal serial para exibir a temperatura de leitura no monitor serial.
+ */
+
+// Definir o pinos do sensor de temperatura e o tipo de sensor
+#include "DHT.h"
+#define DHTPIN A5 // pino do sensor de temperatura
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
 
 // Definir os pinos dos botões e do LED
-#define LED_AZUL 4           // pino do LED azul
-#define LED_AMARELO 3        // pino do LED amarelo
-#define LED_VERMELHO 2       // pino do LED vermelho
-#define botao 5              // pino do botão
-#define sensorTemperatura A5 // pino do sensor de temperatura
+#define LED_AZUL 4     // pino do LED azul
+#define LED_AMARELO 3  // pino do LED amarelo
+#define LED_VERMELHO 2 // pino do LED vermelho
+#define botao 5        // pino do botão
 
 // Variáveis
 int s = 1;                     // estado do sistema
@@ -26,15 +39,15 @@ void setup()
     pinMode(LED_VERMELHO, OUTPUT); // define o pino do LED vermelho como saída
     pinMode(botao, INPUT);         // define o pino do botão como entrada
     Serial.begin(9600);            // inicia a comunicação serial
+    dht.begin();
 }
 
 void loop()
 {
-    estadoBotao = digitalRead(botao);                // lê o estado do botão
-    int valorSensor = analogRead(sensorTemperatura); // lê o valor do sensor de temperatura
-    float voltage = (valorSensor * 5.0) / 1024.0;    // converte o valor lido para tensão
-    temperatura = (voltage - 0.5) * 100;             // converte a tensão para temperatura em graus Celsius
-    if (temperatura != temperaturaAnterior)          // se a temperatura for diferente da temperatura anterior
+    estadoBotao = digitalRead(botao);    // lê o estado do botão
+    temperatura = dht.readTemperature(); // lê o valor do sensor de temperatura
+
+    if (temperatura != temperaturaAnterior) // se a temperatura for diferente da temperatura anterior
     {
         temperaturaAnterior = temperatura; // atualiza a temperatura anterior
         Serial.print("Temperatura:");      // exibe a mensagem no monitor serial
